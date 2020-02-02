01a47c0889064fc7344f65bb483ab9c89ff04555
peterdors <peterdorsaneo@gmail.com>
2020-02-02 09:20:56 -0500

    removing personal assignments folder

--- Assignments/ProgrammingAssignment1/part1/backup/run.sh
+++
@@ -1,5 +0,0 @@
-g++ -std=c++11 -pthread primes.cpp
-./a.out
-
-# Use this to get system runtime.
-# /usr/bin/time ./a.out
--- Assignments/ProgrammingAssignment1/part1/run.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-
-# This script is just for running the primes.cpp program quickly and easily.
-g++ -std=c++11 -pthread primes.cpp && ./a.out
-
